8e4ca9d788ee59d66d38ed2cd7e428f494a213ea
sergei <ravinski.sergei@gmail.com>
2023-03-15 12:56:56 +0100

    Added guitars

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,9 +1,7 @@
-import guitar from "./documents/guitar.js"
 import product from "./documents/product.js"
 import category from "./documents/category.js"
 
 export default [
-	guitar,
 	product,
 	category
 ]
