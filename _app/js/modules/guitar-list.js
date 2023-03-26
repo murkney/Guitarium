ff298d7b7f447f32872150828f4e0e83f32bff60
Sergei_ravinski <Ravinski.sergei@gmail.com>
2023-03-26 23:06:24 +0200

    Added function to filter guitars (#7)

--- _app/js/modules/guitar-list.js
+++ _app/js/modules/guitar-list.js
@@ -12,59 +12,50 @@ export default async function guitarList() {
 	// const params = {};
 
 	const guitars = await sanity.fetch(query);
-	
 
 	function createGuitarListContainerDOM() {
 
 		let guitarListContainer = '';
 
-		//guitars.filter(event => { 
-		//	return event.category.name === type;
-		//});
-		
-		//if (guitars.category === 'Electric guitar') {
-		//	guitarListContainer = document.querySelector('.section__main-electric-guitar');
-		//}
-		//if (guitars.category === 'Bass guitar') {
-		//	guitarListContainer = document.querySelector('.section__main-bass-guitar');
-		//}
-		//if (guitars.category === 'Acoustic guitar') {
-		//	guitarListContainer = document.querySelector('.section__main-acoustic-guitar');
-		//}
+		guitars.filter(guitar => { 
 
+			const filter = guitar.category.name;
 
-		guitarListContainer = document.querySelector('.section__main-electric-guitar');
+			if (filter === 'Electric guitar') {
+				guitarListContainer = document.querySelector('.section__main-electric-guitar');
+			}
+			if (filter === 'Bass guitar') {
+				guitarListContainer = document.querySelector('.section__main-bass-guitar');
+			}
+			if (filter === 'Acoustic guitar') {
+				guitarListContainer = document.querySelector('.section__main-acoustic-guitar');
+			}
 
-		for (const guitar of guitars) {
-
-			//if(guitar.category.name === 'Bass guitar guitar') {
+			const guitarListItem = document.createElement('div');
+			const guitarListItemImage = document.createElement('img');
+			const guitarListItemSpan = document.createElement('span');
+			const guitarListItemTitle = document.createElement('h3');
+			const guitarListItemPrice = document.createElement('h4');
+			const guitarListItemHover = document.createElement('div');
 			
-				const guitarListItem = document.createElement('div');
-				const guitarListItemImage = document.createElement('img');
-				const guitarListItemSpan = document.createElement('span');
-				const guitarListItemTitle = document.createElement('h3');
-				const guitarListItemPrice = document.createElement('h4');
-				const guitarListItemHover = document.createElement('div');
-				
-				guitarListItem.className = 'section__main-electric-guitar-card';
-				guitarListItemHover.className = 'section__main-electric-guitar-card-hover'
+			guitarListItem.className = 'section__main-electric-guitar-card';
+			guitarListItemHover.className = 'section__main-electric-guitar-card-hover'
 
-				guitarListItemImage.src = guitar.image;
-				guitarListItemTitle.innerText = `${guitar.brand} ${guitar.model}`;
-				guitarListItemPrice.innerText = `$ ${guitar.price}`;
-				
-				guitarListContainer.appendChild(guitarListItem);
-				guitarListItem.append(
-					guitarListItemImage,
-					guitarListItemSpan
-				);
-				guitarListItemSpan.append(
-					guitarListItemTitle,
-					guitarListItemPrice
-				);
-				guitarListItem.appendChild(guitarListItemHover);
-			//}
-		}
+			guitarListItemImage.src = guitar.image;
+			guitarListItemTitle.innerText = `${guitar.brand} ${guitar.model}`;
+			guitarListItemPrice.innerText = `$ ${guitar.price}`;
+			
+			guitarListContainer.appendChild(guitarListItem);
+			guitarListItem.append(
+				guitarListItemImage,
+				guitarListItemSpan
+			);
+			guitarListItemSpan.append(
+				guitarListItemTitle,
+				guitarListItemPrice
+			);
+			guitarListItem.appendChild(guitarListItemHover);
+		});
 		return guitarListContainer;
 	}
 
