ea9ef5ba7cb8b35597553ed749d23fec26a41d43
MarkusVaa <36377174+murkney@users.noreply.github.com>
2023-04-13 18:28:11 +0200

    fixed in stock page (#15)

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,8 +1,10 @@
+import inStock from "./modules/inStock.js";
 import guitarList from "./modules/guitar-list.js";
 import aside from "./modules/aside.js";
 import scrollIntoSection from "./modules/scroll_into_section.js";
 import Search from "./modules/search.js";
 
+inStock();
 guitarList();
 aside();
 scrollIntoSection();
---
+++ _app/js/modules/inStock.js
@@ -0,0 +1,60 @@
+import { sanity } from "../sanity.js";
+
+export default async function inStock() {
+	const query = `
+		*[_type == 'in_stock'] {
+			title,
+			available-> {
+				_id,
+				'brand': brand->name,
+				model,
+				price,
+				'image': gallery[0].asset->url,
+				"category": category->{name},
+				string_count
+			}
+		}
+	`;
+
+	const inStockedItems = await sanity.fetch(query);
+
+	function createInStockListContainerDOM() {
+		for (const inStockedItem of inStockedItems) {
+			const inStockContainer = document.getElementById('instock');
+
+			const inStockItem = document.createElement('div');
+			const inStockItemImage = document.createElement('img');
+			const inStockItemSpan = document.createElement('span');
+			const inStockItemTitle = document.createElement('h3');
+			const inStockItemPrice = document.createElement('h4');
+			const inStockItemLocation = document.createElement('h4');
+			const inStockItemHover = document.createElement('div');
+			
+			inStockItem.className = 'section__main-guitars-electric-guitar-card';
+			inStockItemHover.className = 'section__main-guitars-electric-guitar-card-hover';
+
+			inStockItemImage.src = inStockedItem.available.image;
+			inStockItemTitle.innerText = `${inStockedItem.available.brand} - ${inStockedItem.available.model}`;
+			inStockItemPrice.innerText = `$ ${inStockedItem.available.price}`;
+			inStockItemLocation.innerText = `${inStockedItem.title}`;
+
+			inStockContainer.appendChild(inStockItem);
+			inStockItem.append(
+				inStockItemImage,
+				inStockItemSpan
+			);
+			inStockItemSpan.append(
+				inStockItemTitle,
+				inStockItemPrice,
+				inStockItemLocation
+			);
+			inStockItem.appendChild(inStockItemHover);
+		}
+	};
+
+	function renderHTML() {
+		createInStockListContainerDOM();
+	}
+
+	renderHTML();
+};
