557538460985e00332d8db0e90d1d52fe2f07f24
sergei <ravinski.sergei@gmail.com>
2023-04-14 11:07:26 +0200

    Changed icon sizes

--- _app/js/main.js
+++ _app/js/main.js
@@ -3,9 +3,11 @@ import GuitarList from "./modules/guitar-list.js";
 import Aside from "./modules/aside.js";
 import ScrollIntoSection from "./modules/scroll_into_section.js";
 import Search from "./modules/search.js";
+import Filtering from "./modules/filtering.js";
 
 InStock();
 GuitarList();
 Aside();
 ScrollIntoSection();
 Search();
+//Filtering();
